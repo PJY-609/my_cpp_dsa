714f040ed75fda2fc30cf75fb02da64f42bd224c
PJY-609 <43508957+PJY-609@users.noreply.github.com>
2020-12-03 17:40:19 +0800

    update list node

---
+++ my_cpp_dsa/list/ListNode.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// list node position
+#define ListNodePosi(T) ListNode<T>* // template alias: #define APtr(T) A<T>*
+
+template <typename T> struct ListNode {
+	T data;
+	ListNodePosi(T) pred, succ; // predecessor and successor
+
+	ListNode(){} // for header and trailer
+	ListNode(T e, ListNodePosi(T) p = NULL, ListNodePosi(T) s = NULL): // initialization list for constructor
+		data(e), pred(p), succ(s){} 
+
+	ListNodePosi(T) insertAsPred(T const &e);
+	ListNodePosi(T) insertAsSucc(T const &e);
+};
+
+#include "ListNode_Implementation.h"
