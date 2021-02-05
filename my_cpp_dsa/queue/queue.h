5da921c8a3b30b366def8587e286ff62084567ca
PJY-609 <43508957+PJY-609@users.noreply.github.com>
2021-02-05 19:00:05 +0800

    Create queue.h

---
+++ my_cpp_dsa/queue/queue.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <../list/List.h>
+
+template <typename T> class Queue : public List<T> {
+public:
+	void enqueue(T const &e) { insertAsLast(e); }
+	T dequeue() { return remove(first()); }
+	T& front() { return first()->data; }
+};
